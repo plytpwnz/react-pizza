2a52aabc6af274a68c15d9a3d9056546b67d1956
plytpwnz <oblomov.v.a@yandex.ru>
2025-05-06 20:54:31 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './scss/app.scss';
+import Categories from './components/Categories';
+import Header from './components/Header';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+
+export default function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
